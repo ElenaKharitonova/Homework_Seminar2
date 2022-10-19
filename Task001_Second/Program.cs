ceb24b98b82d85b6295cf9f441179f541db5cfe9
KharitonovaElena <lenuciya@mail.ru>
2022-10-19 22:57:56 +0300

    Find second digit

--- Task001_Second/Program.cs
+++ Task001_Second/Program.cs
@@ -1,4 +1,10 @@
-﻿int Prompt(string message)
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа. 
+// Не использовать строки для расчета.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int Prompt(string message)
 {
     Console.Write(message);
     int number = Convert.ToInt32(Console.ReadLine());
