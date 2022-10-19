f3fe71c48eeb46141ead9f1a2e1964e7f87cd82a
KharitonovaElena <lenuciya@mail.ru>
2022-10-19 22:51:36 +0300

    Find secod number

---
+++ Task001_Second/Program.cs
@@ -0,0 +1,23 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+bool ValidateNumber(int number)
+{
+    if (number < 100 || number >= 1000)
+    {
+        Console.WriteLine("Это число не трехзначное");
+        return false;
+    }
+    return true;
+}
+int number = Prompt("Введите трехзначное число: ");
+if (ValidateNumber(number))
+{
+    int firstDigit = number / 10;
+    int lastDigit = firstDigit % 10;
+    Console.WriteLine($"Вторая цифра числа {number} равна {lastDigit}");
+}
+
