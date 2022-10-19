392ff12e18ee90c30f8882f22c1731ff177f48fa
KharitonovaElena <lenuciya@mail.ru>
2022-10-20 01:28:54 +0300

    Weekend?

---
+++ Task004_Weekend/Program.cs
@@ -0,0 +1,34 @@
+﻿// // Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+bool ValidateNumber(int number)
+{
+    if (number < 1 || number > 7)
+    {
+        Console.WriteLine("Введенное число не соответствует дню недели");
+        return false;
+    }
+    return true;
+}
+int number = Prompt("Введите число от 1 до 7, соответствующее дню недели: ");
+if (ValidateNumber(number))
+{
+    if (number > 1 && number < 6)
+    {
+        Console.WriteLine($"{number} не является выходным днем");
+    }
+    else
+    {
+        Console.WriteLine($"{number} является выходным днем");
+    }
+}
